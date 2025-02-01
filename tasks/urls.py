6fbea8cf1b4be62fe561daba8c35638e70df80a8
Charles Ngatia <cngatia1@gmail.com>
2025-02-01 19:34:04 +0300

    urls tasks added

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('tasks/<int:pk>/complete/', TaskCompleteToggleView.as_view(), name='task-complete-toggle'),
+    path('tasks'),
 ]
